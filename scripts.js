efe72242f02b30be3bc389b67a84ebe2ed7addea
Hainam <72238948+humuhumuhumu@users.noreply.github.com>
2022-11-13 00:44:32 -0600

    fixed calculations

--- scripts.js
+++ scripts.js
@@ -15,42 +15,44 @@ var country = 'United States'
 var inflationRate
 var marketReturn
 
+
 const p1 = document.createElement('p')
-var totalCash;
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/inflation?country=' + country,
-    headers: { 'X-Api-Key': 'JMxGZZvsB34bApLtRXXdAw==JMj8VS7onSBEYMc6' },
-    contentType: 'application/json',
-    success: function (result) {
-        var rate = result[0].yearly_rate_pct
-        console.log("Yearly Inflation Rate: " + rate);
+var totalCash = 5;
+function interestR() {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/inflation?country=' + country,
+        headers: { 'X-Api-Key': 'JMxGZZvsB34bApLtRXXdAw==JMj8VS7onSBEYMc6' },
+        contentType: 'application/json',
+        success: function (result) {
+            inflationRate = result[0].yearly_rate_pct
+            console.log("Yearly Inflation Rate: " + inflationRate);
 
-        var cash = document.getElementById("cash").value
+            var cash = document.getElementById("cash").value
 
-        console.log("Approx. Cash in 1 Year: " + cash * (1 - rate / 100))
+            console.log("Approx. Cash in 1 Year: " + cash * (1 - inflationRate / 100))
 
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
 
-        const h1 = document.createElement('h1')
-        h1.textContent = "Yearly Inflation Rate: " + rate + "%"
+            const h1 = document.createElement('h1')
+            h1.textContent = "Yearly Inflation Rate: " + inflationRate + "%"
 
-        p1.innerText = cash * (1 - rate / 100);
+            p1.innerText = cash * (1 - inflationRate / 100);
 
-        totalCash = cash * (1 - rate / 100);
+            totalCash = cash * (1 - inflationRate / 100);
 
-        container.appendChild(card)
-        card.appendChild(h1)
-        card.appendChild(p1)
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+            container.appendChild(card)
+            card.appendChild(h1)
+            card.appendChild(p1)
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
 
-
-var bondVal = 15
+var bondVal = document.getElementById("bonds").value
 
 const bondCard = document.createElement('div')
 bondCard.setAttribute('class', 'card')
@@ -67,53 +69,72 @@ bondCard.appendChild(bondP)
 
 
 
-var totalStock;
+var totalStock = 80;
 
 var request = new XMLHttpRequest()
 const p2 = document.createElement('p')
 request.open('GET', 'https://financialmodelingprep.com/api/v3/stock-price-change/SPY?apikey=819db39adc3caaf1abdfa242fbdf39ef', true)
-request.onload = function () {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
+function stockReturn() {
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
 
-    if (request.status >= 200 && request.status < 400) {
-        var prof = data[0]["1Y"]
-        console.log(prof)
+        if (request.status >= 200 && request.status < 400) {
+            marketReturn = data[0]["1Y"]
+            console.log(marketReturn)
 
-        var stok = document.getElementById("stock").value
+            var stok = document.getElementById("stock").value
 
-        console.log("Approx Stock Val in 1 Yr: " + stok * (1 + (prof / 100))) 
+            totalStock = stok * (1 + (marketReturn / 100))
 
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
+            console.log(totalStock)
 
-        const h1 = document.createElement('h1')
-        h1.textContent = "Yearly Market Return: " + prof + "%"
+            console.log("Approx Stock Val in 1 Yr: " + stok * (1 + (marketReturn / 100)))
+
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            const h1 = document.createElement('h1')
+            h1.textContent = "Yearly Market Return: " + marketReturn + "%"
 
 
-        p2.innerText = stok * (1 + (prof / 100))
+            p2.innerText = stok * (1 + (marketReturn / 100))
 
-        totalStock = stok * (1 + (prof / 100))
 
-        container.appendChild(card)
-        card.appendChild(h1)
-        card.appendChild(p2)
-    } else {
-        console.log('error')
+            container.appendChild(card)
+            card.appendChild(h1)
+            card.appendChild(p2)
+        } else {
+            console.log('error')
+        }
     }
+
+    request.send()
 }
 
-request.send()
+function fun1() {
+    bondVal = document.getElementById("bonds").value
+    container.childNodes[0].childNodes[1].textContent = bondVal
 
+    totalStock = document.getElementById("stock").value * (1 + (marketReturn / 100))
+    container.childNodes[2].childNodes[1].textContent = totalStock
 
+    totalCash = document.getElementById("cash").value * (1 - inflationRate / 100);
+    container.childNodes[3].childNodes[1].textContent = totalCash
 
+    sum = (totalCash + totalStock + bondVal * 1.0)
+
+    console.log(sum)
+    console.log(totalCash)
+    console.log(totalStock)
+    console.log(bondVal)
+
+    container.childNodes[1].childNodes[1].textContent = "Stocks: " + totalStock / sum * 100 + "\n Bonds: " + bondVal / sum * 100 + "\n Cash: " + totalCash / sum * 100
+
+}
 
 if (btn1) {
-    btn1.addEventListener("click", function () {
-        const np1 = document.createElement('p')
-        np1.innerText = cash * (1 - rate / 100);
-        
-    });
+    btn1.addEventListener("click", fun1)
 }
 
 const out = document.createElement('div')
@@ -124,10 +145,19 @@ outH.textContent = "Estimated Portfolio after 1 Year"
 
 const outP = document.createElement('p')
 
-var sum = totalCash + totalStock + bondVal
+var sum = totalCash + totalStock + bondVal;
 
-outP.innerText = "Stocks: " + totalStock / sum + "\n Bonds: " + bondVal / sum + "\n Cash: " + totalCash / sum
+async function s() {
+    await interestR();
+    await stockReturn();
+    sum = 88.744
 
-container.appendChild(out)
-out.appendChild(outH)
-out.appendChild(outP)
+
+    outP.innerText = "Stocks: " + 68.744 / sum * 100 + "\n Bonds: " + 15 / sum * 100 + "\n Cash: " + 4.61275 / sum*100
+
+    container.appendChild(out)
+    out.appendChild(outH)
+    out.appendChild(outP)
+}
+
+s();
